5e25b16dea48272e68c686a6f12f5b0eb323b195
Matthew Harrold <github@marrold.co.uk>
2021-11-10 13:57:07 +0000

    Bump tag in example

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,6 @@
 module "dns" {
   
-  source = "github.com/marrold/terraform-rfc2136-dns-yaml?ref=v0.4"
+  source = "github.com/marrold/terraform-rfc2136-dns-yaml?ref=v0.5"
 
   zone_yaml_dirs  = ["./zones1", "./zones2"]
   default_ttl = 600
